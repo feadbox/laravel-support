f6be737b85936802f3e79674340ba170cf4423f0
Davut Kember <davutkmbr@gmail.com>
2021-08-06 01:04:53 +0300

    Add float for setting value

--- src/Casts/MoneyCast.php
+++ src/Casts/MoneyCast.php
@@ -2,18 +2,19 @@
 
 namespace Feadbox\Support\Casts;
 
-use Feadbox\Support\Models\Money;
+use Feadbox\Support\Components\Money as MoneyComponent;
+use Feadbox\Support\Models\Money as MoneyModel;
 use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
 
 class MoneyCast implements CastsAttributes
 {
     public function get($model, $key, $value, $attributes)
     {
-        return new Money(['price' => $value]);
+        return new MoneyModel(['price' => $value]);
     }
 
     public function set($model, $key, $value, $attributes)
     {
-        return $value;
+        return MoneyComponent::toFloat($value);
     }
 }
