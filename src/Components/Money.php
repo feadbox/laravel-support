a081ea01b4b70d74477eaca6e7fcae70c0d67e90
Davut Kember <davutkmbr@gmail.com>
2021-08-06 01:01:17 +0300

    Add preg_replace for non-numeric chars.

--- src/Components/Money.php
+++ src/Components/Money.php
@@ -14,18 +14,20 @@ class Money
         return static::format($money, '');
     }
 
-    public static function makeFloat($money): float
+    public static function toFloat($money): float
     {
         if (is_null($money)) {
             return 0;
         }
+
+        $money = preg_replace('/[^0-9]/', '', $money);
 
         return (float) str_replace(',', '', $money);
     }
 
     public static function convertToCents($money): ?float
     {
-        return !is_null($money) ? static::makeFloat($money) * 100 : NULL;
+        return !is_null($money) ? static::toFloat($money) * 100 : NULL;
     }
 
     public static function convertFromCents($money)
